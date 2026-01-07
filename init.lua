e51c51620616d91ce0407c37f212b9e7c1af2017
1F616EMO <root@1f616emo.xyz>
2026-01-07 22:23:48 +0800

    init

---
+++ init.lua
@@ -0,0 +1,39 @@
+-- monitoring_player_information
+-- Sends player information to monitoring
+-- SPDX-LICENSE-IDENTIFIER: MIT
+
+core.action("[monitoring_player_information] Enabling player information logging")
+
+local monitoring_counters = {}
+
+for field, description in ipairs({
+    ["protocol_version"] = "protocol version",
+    ["formspec_version"] = "formspec version",
+}) do
+    monitoring_counters[field] = {
+        all = monitoring.counter(
+            "player_information_" .. field,
+            "Number of player joins by " .. description,
+            { labels = { field } }
+        ),
+        new = monitoring.counter(
+            "player_information_" .. field .. "_new",
+            "Number of new player joins by " .. description,
+            { labels = { field } }
+        ),
+    }
+end
+
+core.register_on_joinplayer(function(player, last_login)
+    local info = core.get_player_information(player:get_player_name())
+
+    for field, counters in pairs(monitoring_counters) do
+        local value = info[field] or 0
+
+        counters.all:inc(1, { [field] = value })
+
+        if not last_login then
+            counters.new:inc(1, { [field] = value })
+        end
+    end
+end)
