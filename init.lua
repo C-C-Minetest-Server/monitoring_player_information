10c0892cd178afde155fc029744997c98b2082d7
1F616EMO <root@1f616emo.xyz>
2026-01-07 22:50:13 +0800

    AI gaslighted me

--- init.lua
+++ init.lua
@@ -4,36 +4,40 @@
 
 core.log("action", "[monitoring_player_information] Enabling player information logging")
 
-local monitoring_counters = {}
+local monitoring_counters = {
+    ["protocol_version"] = { all = {}, new = {} },
+    ["formspec_version"] = { all = {}, new = {} },
+}
 
-for field, description in ipairs({
-    ["protocol_version"] = "protocol version",
-    ["formspec_version"] = "formspec version",
-}) do
-    monitoring_counters[field] = {
-        all = monitoring.counter(
-            "player_information_" .. field,
-            "Number of player joins by " .. description,
-            { labels = { field } }
-        ),
-        new = monitoring.counter(
-            "player_information_" .. field .. "_new",
-            "Number of new player joins by " .. description,
-            { labels = { field } }
-        ),
-    }
+local function increment_counter(field, value, new)
+    local counters = monitoring_counters[field][value]
+    if not counters then
+        counters = {
+            all = monitoring.counter(
+                "player_information_" .. field,
+                "Number of player joins by " .. field,
+                { labels = { [field] = value } }
+            ),
+            new = monitoring.counter(
+                "player_information_" .. field .. "_new",
+                "Number of new player joins by " .. field,
+                { labels = { [field] = value } }
+            ),
+        }
+        monitoring_counters[field][value] = counters
+    end
+
+    counters.all:inc()
+    if new then
+        counters.new:inc()
+    end
 end
 
 core.register_on_joinplayer(function(player, last_login)
     local info = core.get_player_information(player:get_player_name())
 
-    for field, counters in pairs(monitoring_counters) do
+    for field in pairs(monitoring_counters) do
         local value = info[field] or 0
-
-        counters.all:inc(1, { [field] = value })
-
-        if not last_login then
-            counters.new:inc(1, { [field] = value })
-        end
+        increment_counter(field, value, not last_login)
     end
 end)
